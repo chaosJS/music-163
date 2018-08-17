71b9c451994b16f8ef461bf393e653a9df531993
chaosJS <lichao779@sina.com>
2018-08-17 20:53:20 +0800

    update basic mvc

---
+++ src/js/song-form.js
@@ -0,0 +1,51 @@
+{
+    let view = {
+        el: '.page>main',
+        template:
+            `
+        <p>歌曲信息</p>
+        <form>
+            <div class="row">
+                <label>
+                    歌名
+                    <br/>
+                    <input type="text">
+                </label>
+
+            </div>
+            <div class="row">
+                <label>
+                    歌手
+                    <br/>
+
+                    <input type="text">
+                </label>
+            </div>
+            <div class="row">
+                <label>
+                    外链
+                    <br/>
+
+                    <input type="text">
+                </label>
+            </div>
+            <div class="row">
+                <button type="submit" class="saveBtn">保存</button>
+            </div>
+        </form>
+        `,
+        render(data) {
+            $(this.el).html(this.template)
+        },
+    }
+    let model = {};
+
+    let controller = {
+        init(view, model) {
+            this.view = view;
+            this.model = model;
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view, model)
+}
---
+++ src/js/song-list.js
@@ -0,0 +1,29 @@
+{
+    let view = {
+        el: '#song-list-container',
+        template: `
+        <ul class="song-list">
+        <li class="active">歌曲12323</li>
+        <li>歌曲1666</li>
+        <li>歌曲14</li>
+        <li>歌曲13</li>
+        <li>歌曲12</li>
+
+    </ul>
+        `,
+        render(data) {
+            $(this.el).html(this.template);
+        }
+    }
+    let model = {};
+
+    let controller = {
+        init(view, model) {
+            this.view = view;
+            this.model = model;
+            this.view.render(this.model.data)
+
+        }
+    }
+    controller.init(view, model)
+}
